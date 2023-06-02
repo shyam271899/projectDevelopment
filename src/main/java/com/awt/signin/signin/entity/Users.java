203398c2fc1f8edbd7d89163db0ba01f45cf21ff
Ashok Reddy Kumbham <ashokreddy.kumbham@gmail.com>
2023-06-02 17:24:47 -0500

    Revert "course list"

--- src/main/java/com/awt/signin/signin/entity/Users.java
+++ src/main/java/com/awt/signin/signin/entity/Users.java
@@ -10,51 +10,15 @@ import lombok.Data;
 @Data
 public class Users {
 
-     @Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
+
     private String lastName;
     private String email;
     private String phoneNumber;
     private String password;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
 
 
 
