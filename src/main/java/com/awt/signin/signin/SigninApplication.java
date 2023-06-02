c5edf2258b03a79c3799c3f939f735fd970d055f
praneethchandranatwa <60193728+praneethchandranatwa@users.noreply.github.com>
2023-06-01 21:43:03 -0500

    Created Registration and login controllers

---
+++ src/main/java/com/awt/signin/signin/SigninApplication.java
@@ -0,0 +1,13 @@
+package com.awt.signin.signin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigninApplication.class, args);
+	}
+
+}
