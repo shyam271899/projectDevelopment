be32cb80c6f35271b18287a9f55e18ca32e387c3
shyam1899 <112034959+shyam1899@users.noreply.github.com>
2023-08-01 17:06:56 -0500

    changes made to pom.xml file and also starting the project

--- src/main/java/com/awt/signin/signin/SigninApplication.java
+++ src/main/java/com/awt/signin/signin/SigninApplication.java
@@ -1,13 +1,16 @@
 package com.awt.signin.signin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SigninApplication {
+public class SigninApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SigninApplication.class, args);
+		new SpringApplicationBuilder(SigninApplication.class).web(WebApplicationType.SERVLET).run(args);
 	}
 
 }
